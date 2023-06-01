a1a6ac2ca364229f4a7fd8796a66e2e182dafb9b
vincentott <vincent.ott@posteo.de>
2023-06-01 11:40:21 +0200

    updated documentation

--- R/pixeltrix.R
+++ R/pixeltrix.R
@@ -8,6 +8,29 @@
 # https://github.com/matt-dray/pixeltrix
 # Date of retrieval: 2023-05-19
 
+# # MIT License
+#
+# Copyright (c) 2022 pixeltrix authors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 
 # The livelycells shiny app only uses click_to_cells() and draw_pixels().
 # see server.R
@@ -17,6 +40,7 @@
 # However, it was useful to develop the package (e.g., to create preset patterns)
 # and should also be useful to maintain the package
 # (e.g., create matrices for unit tests).
+
 
 
 # Helper functions -------------------------------------------------------------
--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # livelycells
-An R Shiny App for Conway´s Game of Life
+An R Shiny App for Conway´s Game of Life  
+[click here to play it in your browser](<https://vincentott.shinyapps.io/livelycells/>)
 
 ## Description
 This is an R package which contains an R Shiny App for
 [Conway´s Game of Life](<http://wikipedia.org/wiki/Conway's_Game_of_Life>).  
-Aside from shiny, livelycells has no dependencies.
-
+<br>
+<br>
+Rules:  
+1. A live cell with two or three live neighbours survives - otherwise it dies.  
+2. A dead cell with three live neighbours becomes a live cell.
+<br>
+<br>
 At time point zero, the player uses their mouse to click some cells to life.  
 Then, the cells evolve over time and show all sorts of interesting behavior.  
-There are also preset patterns.
+There are also preset patterns which can be loaded.
 
-This package was build with the help of code from the
+
+# Dependencies, Credits, Copyright
+Aside from shiny, livelycells has no dependencies.  
+However, this app was built with the help of code under the MIT license from the
 [pixeltrix package](<http://github.com/matt-dray/pixeltrix>) by [Matt Dray](<http://matt-dray.com/>).
